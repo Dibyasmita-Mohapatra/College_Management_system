8f5b1ba4a2cad0db1479bd2964e55e1fc56b83e8
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-02-26 16:57:36 +0530

    Create Student Profile

--- backend/src/controllers/studentController.js
+++ backend/src/controllers/studentController.js
@@ -1,1 +1,22 @@
+//Get Student Profile
 
+exports.getStudentProfile = async (req, res) => {
+  try {
+    const userid = req.user.userid;
+
+    const [rows] = await db.query(
+      "SELECT * FROM students WHERE userid = ?",
+      [userid]
+      );
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json(rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching profile" });
+  }
+};
+    
+    
