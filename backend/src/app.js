0da437341bbcff500c07f18eb12310f652616190
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-03-17 16:13:29 +0530

    Connect student profile routes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ const studentRoutes = require("./routes/studentRoutes");
 const attendanceRoutes = require("./routes/attendanceRoutes");
 const marksRoutes = require("./routes/marksRoutes");
 const studentDashboardRoutes = require("./routes/studentDashboardRoutes");
+const studentProfileRoutes = require("./routes/studentProfileRoutes");
 
 const app = express();
 
