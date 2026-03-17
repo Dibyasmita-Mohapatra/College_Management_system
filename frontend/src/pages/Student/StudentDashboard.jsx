a522e6b2d968f1db479921ec299d620386ac322d
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-03-17 16:37:43 +0530

    feat: fetch student dashboard data and manage loading state

--- frontend/src/pages/Student/StudentDashboard.jsx
+++ frontend/src/pages/Student/StudentDashboard.jsx
@@ -1,10 +1,37 @@
-const StudentDashboard = () => {
-    return (
-        <div style={{ padding: "40px" }}>
-            <h1>Welcome Student</h1>
-            <p>This is the Student Dashboard.</p>
-        </div>
-    );
-};
+import { useEffect, useState } from "react";
+import api from "../../utils/api";
 
-export default StudentDashboard;
+const StudentDashboard = () => {
+
+    const token = localStorage.getItem("token");
+
+    const [student, setStudent] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const fetchStudent = async () => {
+
+            try {
+
+                const res = await api.get(
+                    "/api/student/dashboard",
+                    {
+                        headers: { Authorization: `Bearer ${token}` }
+                    }
+                );
+
+                setStudent(res.data);
+
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+
+        };
+
+        if (token) fetchStudent();
+
+    }, [token]);
+    
