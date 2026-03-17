ca265f7de0e2e59c8b6c71e7b196c86308276af8
Dibyasmita-Mohapatra <dibyasmita.m05@gmail.com>
2026-03-17 17:22:08 +0530

    feat(student-profile): setup StudentProfile component with state

--- frontend/src/pages/Student/StudentProfile.jsx
+++ frontend/src/pages/Student/StudentProfile.jsx
@@ -1,1 +1,9 @@
+import { useState } from "react";
+import api from "../../utils/api";
 
+const StudentProfile = () => {
+
+    const token = localStorage.getItem("token");
+
+    const [password, setPassword] = useState("");
+    const [dob, setDob] = useState("");
